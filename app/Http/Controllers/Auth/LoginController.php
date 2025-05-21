578f4540dd309876e7cd8c3452857f02c44d9bed
Slophil <slophil.contact@gmail.Com>
2025-05-21 17:57:09 +0200

    WIP: 🔒 Add authentication APIs and dependencies
        Added functionalities for authentication endpoints (`Logout` and `Register`) that interact with the API.
        Updated `package-lock.json` to include necessary dependencies for TailwindCSS, Axios, and Vite.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -10,25 +11,46 @@ class LoginController extends Controller
     /**
      * Handle a login request to the application.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @return JsonResponse
      */
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
-        // Validate the request...
-        // Attempt to log the user in...
-        // Return a response...
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (auth()->attempt($credentials)) {
+            // Authentication passed...
+            $user = auth()->user();
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Login successful',
+                'data' => $user,
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
     }
 
     /**
      * Handle a logout request to the application.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @return JsonResponse
      */
-    public function logout(Request $request)
+    public function logout(Request $request): JsonResponse
     {
-        // Invalidate the user's session...
-        // Return a response...
+        auth()->logout();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Logout successful',
+        ], 200);
     }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -22,7 +22,7 @@ class RegisterController extends Controller
     {
 
         $validated = $request->validate([
-            'username' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
