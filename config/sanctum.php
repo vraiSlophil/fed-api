4484a4f3f25fe9b5e3fbb57f171efbea17d7f053
nathan <slophil.contact@gmail.com>
2026-01-06 18:00:35 +0100

    refactor(api): improve API route formatting and authentication flow
    Refactor the API routes for better readability and consistency.
    Updated the authentication flow in the LoginRequest and LogoutController
    to ensure a stateless approach using Bearer tokens, removing reliance
    on user resolvers and sessions.
    
    Refs #8

--- config/sanctum.php
+++ config/sanctum.php
@@ -3,7 +3,6 @@
 use Laravel\Sanctum\Sanctum;
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Stateful Domains
@@ -15,12 +14,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:3000,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
-    ))),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', '')),
 
     /*
     |--------------------------------------------------------------------------
@@ -80,5 +74,4 @@ return [
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
         'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
-
 ];
