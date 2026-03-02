321529b1588e8bc39b8cac8a82e8835bcbbcfe9f
nathan <slophil.contact@gmail.com>
2026-03-02 09:07:31 +0100

    feat(auth): enhance password validation rules in registration request

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Auth;
 
 use App\Models\Auth\User;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rules;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
@@ -28,7 +28,7 @@ class RegisterRequest extends FormRequest
         return [
             'username' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed', Password::min(8)->mixedCase()->numbers()->symbols()->uncompromised()],
         ];
     }
 }
